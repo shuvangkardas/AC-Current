c9662e79e42db51d9cf11956551fbb27cd1ffbd2
Shuvangkar Chandra Das <7550796+shuvangkar@users.noreply.github.com>
2017-08-01 03:47:41 +0600

    Add files via upload

---
+++ PowerAnalyzer.h
@@ -0,0 +1,38 @@
+/*
+  * This library is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU General Public License.
+  * The software is published by www.eeetechbd.com.
+  * The library is devloped by Shuvangkar Chandra Das(Shuvangkar Shuvo) 
+  * founder of eeeTechBd
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+  */
+#ifndef POWERANALYZER_h
+#define POWERANALYZER_h
+#include "Arduino.h"
+#define N 200
+
+class Measure
+{
+ 
+  //Library interface description 
+  public:
+    Measure(int VoltPin,int CurrentPin);
+    float RMS(int channel);
+    float power();
+    float voltage();
+    float current();
+    float PowerFactor();
+    void all(float &voltage, float &current,float &power, float &PF);
+    float PhaseAngle();
+    void ADC_Initialize();
+    unsigned int ADC_Read(uint8_t channel);
+    
+   private:
+      int _VoltPin;
+      int _CurrentPin; 
+};
+
+#endif
+
